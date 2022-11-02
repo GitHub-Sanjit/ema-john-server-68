db0e5992ab243ecb3896ee46ef0d03f03578cd21
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-11-02 22:01:34 +0600

    68-1 Module Overview and set simple ema-john server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zevqp.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.get("/", (req, res) => {
   res.send("ema john server is running");
 });
@@ -15,3 +29,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`ema john running on port ${port}`);
 });
+
+// emaJohnDbUser
+// E9k3D1wZA9bHuOvE
